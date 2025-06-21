f346abd3d4ae311a2e22b2771b14656ba44c5101
Deven Shona <51067067+spacemex@users.noreply.github.com>
2025-06-20 19:57:20 -0500

    Added client setup event handling and logging for skill scanning initialization

--- src/main/java/com/github/spacemex/skillsexpnotifier/Skillsexpnotifier.java
+++ src/main/java/com/github/spacemex/skillsexpnotifier/Skillsexpnotifier.java
@@ -2,15 +2,27 @@ package com.github.spacemex.skillsexpnotifier;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.nio.file.Path;
 
 @Mod(Skillsexpnotifier.MODID)
 public class Skillsexpnotifier {
     public static final String MODID = "skillsexpnotifier";
-    public Skillsexpnotifier() {
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
+    public Skillsexpnotifier(FMLJavaModLoadingContext context) {
         MinecraftForge.EVENT_BUS.register(this);
+        context.getModEventBus().addListener(this::onClientSetup);
+
+        Config.register(context);
+    }
+
+    private void onClientSetup(FMLClientSetupEvent event) {
+        Skillsexpnotifier.LOGGER.info("Mods are loaded, now scanning for skills…");
         Path configDir = FMLPaths.CONFIGDIR.get();
         EntryRegistry.loadFromFile(configDir);
     }
